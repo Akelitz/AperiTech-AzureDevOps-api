a4f31b0aab99b51bd1caf908f5e7831adcc0e2f1
Giorgio Di Nardo <akelitz@hotmail.com>
2018-10-05 19:58:51 +0200

    Change 1

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,24 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AperiTech_AzureDevOps_api.Controllers
+namespace AperiTech_AzureDevOps_test.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
+            return DateTime.Now.ToString();
         }
 
         // POST api/values
--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace AperiTech_AzureDevOps_api
+namespace AperiTech_AzureDevOps_test
 {
     public class Startup
     {
@@ -25,6 +25,7 @@ namespace AperiTech_AzureDevOps_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -41,6 +42,7 @@ namespace AperiTech_AzureDevOps_api
             }
 
             app.UseHttpsRedirection();
+            app.UseCors();
             app.UseMvc();
         }
     }
